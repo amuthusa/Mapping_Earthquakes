a2cb30ff803c51c6f5b6762f6136387e4705b935
Ayyappa Muthusami <ayyappamuthusami@Ayyappas-MacBook-Pro.local>
2020-04-08 23:22:09 -0700

    added multiple lines using polyline

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -3,11 +3,11 @@ console.log("working");
 
 // Create the map object with a center and zoom level.
 //let map = L.map('mapid').setView([34.0522, -118.2437], 14);
-let map = L.map('mapid').setView([37.0902, -95.7129], 4);
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
 //creating a tile layer which will be set as a background for our map
 // https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: MAP_API_KEY
@@ -31,3 +31,27 @@ cityData.forEach(function (city) {
     .addTo(map);
 });
 
+//add points for LAX airport to SFO airport
+let coordinates = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+];
+
+//create a polyline using coordinates
+L.polyline(coordinates, {
+    color: "yellow"
+}).addTo(map);
+
+//From SFO to JFK via AUS and YYZ
+coordinates = [
+    [37.6213, -122.3790],
+    [30.1975, -97.6664],
+    [43.6777, -79.6248],
+    [40.6413, -73.7781]
+];
+
+L.polyline(coordinates, {
+    color: "blue"
+}).addTo(map);
